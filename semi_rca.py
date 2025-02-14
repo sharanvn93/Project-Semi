eff2badacf3d67f461637a1ed383d726ab81b31c
sharanvn93 <85973996+sharanvn93@users.noreply.github.com>
2025-02-14 14:42:58 -0800

    Update semi_rca.py

--- semi_rca.py
+++ semi_rca.py
@@ -14,27 +14,27 @@ def main():
         
         # Content to be added later
     
-        elif tab_selection == "Parts History":
+    elif tab_selection == "Parts History":
         
         
         # Create two columns for layout
-            col1, col2 = st.columns([1, 1])
+        col1, col2 = st.columns([1, 1])
         
-            with col1:
-                st.markdown("---")  # Horizontal line for division
-                st.markdown("<h3 style='text-align: center;'>Critical Parts RFH</h3>", unsafe_allow_html=True)
-                # Upper part content to be added
-                st.markdown("---")  # Horizontal line for division
-                st.markdown("<h3 style='text-align: center;'>Parts Install</h3>", unsafe_allow_html=True)
-                # Lower part content to be added
+        with col1:
+            st.markdown("---")  # Horizontal line for division
+            st.markdown("<h3 style='text-align: center;'>Critical Parts RFH</h3>", unsafe_allow_html=True)
+            # Upper part content to be added
+            st.markdown("---")  # Horizontal line for division
+            st.markdown("<h3 style='text-align: center;'>Parts Install</h3>", unsafe_allow_html=True)
+            # Lower part content to be added
         
-            with col2:
-                st.markdown("<h3 style='text-align: center;'>Current Config</h3>", unsafe_allow_html=True)
-                # Right column content to be added
+        with col2:
+            st.markdown("<h3 style='text-align: center;'>Current Config</h3>", unsafe_allow_html=True)
+            # Right column content to be added
+    
+    elif tab_selection == "Process Run Signals":
         
-        elif tab_selection == "Process Run Signals":
-        
-            # Content to be added later
+        # Content to be added later
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     main()
