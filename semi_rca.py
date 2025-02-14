8d29cb7f606d34ff3db0ae4466137a07f882cebc
sharanvn93 <85973996+sharanvn93@users.noreply.github.com>
2025-02-14 15:38:21 -0800

    Update semi_rca.py

--- semi_rca.py
+++ semi_rca.py
@@ -18,11 +18,17 @@ def main():
         col1, spacer, col2 = st.columns([1, 0.005, 1])
         
         with col1:
-            st.markdown("<h3 style='text-align: center;'>Critical Parts RFH</h3>", unsafe_allow_html=True)
+            st.markdown("<h3 style='text-align: center; padding-bottom: 10px;'>Critical Parts RFH</h3>", unsafe_allow_html=True), unsafe_allow_html=True)
             # Upper part content to be added
             st.markdown("---")  # Horizontal line for division
-            st.markdown("<h3 style='text-align: center;'>Parts Install</h3>", unsafe_allow_html=True)
-            # Lower part content to be added
+            st.markdown("<h3 style='text-align: center; padding-bottom: 10px;'>Parts Install</h3>", unsafe_allow_html=True), unsafe_allow_html=True)
+            col_lower = st.columns([1, 1, 1])
+            with col_lower[0]:
+                st.button("Mini PM")
+            with col_lower[1]:
+                st.button("Major PM")
+            with col_lower[2]:
+                st.button("Upgrade")
         
         with spacer:
             st.markdown("<div style='border-left: 2px solid black; height: 100vh;'></div>", unsafe_allow_html=True)
@@ -36,4 +42,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
